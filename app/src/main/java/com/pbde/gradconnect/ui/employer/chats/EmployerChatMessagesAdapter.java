06dd19fab5eaab409e0aa9b3dad4a3025c5300b3
Engeliiica <amavundlas1@gmail.com>
2025-05-18 22:15:51 +0200

    first commit

---
+++ app/src/main/java/com/pbde/gradconnect/ui/employer/chats/EmployerChatMessagesAdapter.java
@@ -0,0 +1,9 @@
+package com.pbde.gradconnect.ui.employer.chats;
+
+import com.pbde.gradconnect.ui.common.BaseChatMessagesAdapter;
+
+public class EmployerChatMessagesAdapter extends BaseChatMessagesAdapter {
+    public EmployerChatMessagesAdapter() {
+        super();
+    }
+}
